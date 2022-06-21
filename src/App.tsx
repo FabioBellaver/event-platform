60bcea998edebf99c4d4d571cccca64148763395
Fábio Bellaver <fabio.bellaver658@gmail.com>
2022-06-20 21:18:34 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <h1 className="text-2xl font-bold text-center pt-10 text-violet-500">
+      Hello World, gonna happen! 🙏
+    </h1>
+  );
+}
+
+export default App;
