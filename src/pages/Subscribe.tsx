559095907f0742c03006480b83c55d3f87f7223c
Fábio Bellaver <fabio.bellaver658@gmail.com>
2022-06-24 20:25:19 -0300

    fixed event image

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { Footer } from "../components/Footer";
 import { Logo } from "../components/Logo";
 import { useCreateSubscriberMutation } from "../graphql/generated";
+import imgUrl from '../../src/assets/code-mockup.png'
 
 export function Subscribe() {
   const navigate = useNavigate();
@@ -75,7 +76,7 @@ export function Subscribe() {
         </div>
       </section>
       <section>
-        <img src="/src/assets/code-mockup.png" className="mt-10" alt="" />
+        <img src={imgUrl} className="mt-10" alt="" />
       </section>
       <Footer />
     </main>
