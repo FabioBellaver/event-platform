7ba054e91067b038c1418300aab970442c541c92
Fábio Bellaver <fabio.bellaver658@gmail.com>
2022-06-24 13:21:13 -0300

    add: event page

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -11,10 +11,81 @@ export function Event() {
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex flex-1">
-        {slug ? <Video lessonSlug={slug} /> : (
-          <div className="flex flex-1 items-center justify-center">
-            <img src="/src/assets/react.png" alt="" className="opacity-40"/>
-            <h1>O poder do React</h1>
+        {slug ? (
+          <Video lessonSlug={slug} />
+        ) : (
+          <div className="flex flex-1 min-h-screen bg-blur bg-cover bg-no-repeat flex-col items-center">
+            <div className="w-full max-w-[1100px] flex items-center mt-96 flex-col">
+              <span className="text-2xl leading-relaxed">
+                <span className="font-bold">Event Platform</span> é um projeto
+                de estudos da{" "}
+                <a
+                  className="text-green-500 hover:text-green-600 transition-colors"
+                  href="https://www.rocketseat.com.br/"
+                >
+                  Rocketseat
+                </a>
+                .
+              </span>
+              <span className="mt-4 text-gray-200 text-md leading-relaxed">
+                Tecnologias usadas no projeto:
+              </span>
+              <ul className="flex mt-4 text-gray-300 gap-4 select-none">
+                <li>
+                  <a
+                    className="hover:text-gray-100 transition-colors"
+                    href="https://reactjs.org/"
+                  >
+                    React
+                  </a>
+                </li>{" "}
+                |
+                <li>
+                  <a
+                    className="hover:text-gray-100 transition-colors"
+                    href="https://vitejs.dev/"
+                  >
+                    Vite
+                  </a>
+                </li>{" "}
+                |
+                <li>
+                  <a
+                    className="hover:text-gray-100 transition-colors"
+                    href="https://www.typescriptlang.org/"
+                  >
+                    TypeScript
+                  </a>
+                </li>{" "}
+                |
+                <li>
+                  <a
+                    className="hover:text-gray-100 transition-colors"
+                    href="https://tailwindcss.com/"
+                  >
+                    TailwindCSS
+                  </a>
+                </li>{" "}
+                |
+                <li>
+                  <a
+                    className="hover:text-gray-100 transition-colors"
+                    href="https://graphql.org/"
+                  >
+                    GraphQL
+                  </a>
+                </li>{" "}
+                |
+                <li>
+                  <a
+                    className="hover:text-gray-100 transition-colors"
+                    href="https://graphcms.com/"
+                  >
+                    GraphCMS
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
         )}
         <Sidebar />
